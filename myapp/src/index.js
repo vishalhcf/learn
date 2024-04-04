66cfa86f61b8ce641b09d01ba1e00665c9dc6610
VISHAL KUMAR <vishalkumarkumar3472@gmail.com>
2024-04-04 13:04:53 +0530

    update

---
+++ myapp/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import  ReactDOM  from 'react-dom/client';
+import './index.css';
+const root=ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+    <div>
+        <h1>This side vishal </h1>
+    </div>
+);
