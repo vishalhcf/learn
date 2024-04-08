344485ce63c8c2f638e0545075f6f11765a4b959
VISHAL KUMAR <vishalkumarkumar3472@gmail.com>
2024-04-08 13:18:00 +0530

    update

--- myapp/src/component/App.js
+++ myapp/src/component/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./Header";
 import TopSection from "./TopSection";
 import Counter from "./Counter";
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 export default function App(){
     return(
         <div>
             <Header/>
             <TopSection/>
-           <br></br>
+            <br></br>
            
             <Counter/>
             
