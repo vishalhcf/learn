489c0c25cddc0153d3f69d11b675a7bf0fd0af65
VISHAL KUMAR <vishalkumarkumar3472@gmail.com>
2024-04-16 13:30:14 +0530

    update effect

--- myapp/src/component/UseEffect.js
+++ myapp/src/component/UseEffect.js
@@ -1,15 +1,32 @@
 import React, { useEffect, useState } from 'react'
 
 export default function UseEffect() {
-  const[counter,setCounter]=useState(0)
-  useEffect(()=>{
-    console.log('mount')
-},[counter]) 
-    return (
-    <div>
-<h1>Counter:{counter}</h1>
-<button className='btn btn-danger' onClick={()=>setCounter(counter+1)}>Inc</button>
 
+  const [counter, setCounter] = useState(0);
+  const [card, setCard] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3004/products')
+      .then(res => res.json())
+      .then(data => {
+        setCard(data)
+      })
+
+  }, [])
+
+  return (
+    <div>
+      <h1>Counter:{counter}</h1>
+      <button className='btn btn-danger' onClick={() => setCounter(counter + 1)}>Inc</button>
+
+    <div className='bg-primary text-white'>
+  {
+    card.map((c)=>{
+      return <li key={c.id}> {c.id}-------{c.pname}-----{c.Price}----{c.instock}</li>
+    })
+  }
+
+    </div>  
     </div>
   )
 }
